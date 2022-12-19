835f34ffb002afaea659fd2427347a8564593317
ZXCSLARK <i.k.inc@mail.ru>
2022-12-19 03:36:53 +0300

    commit

--- scripts/checkValue.js
+++ scripts/checkValue.js
@@ -3,7 +3,7 @@ function validate(){
     let name = document.forms["feedback"]["name"].value;
     let email = document.forms["feedback"]["email"].value;
 
-    if(name.trim().length || email.trim().length) {
+    if(name.trim().length == 0 || email.trim().length == 0) {
         alert('Пожалуйста, заполните все поля');
         return false;
     }
